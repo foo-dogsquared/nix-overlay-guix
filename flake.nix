efa203a2ba1bfac171ccb2be4248b67a029729cd
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-03-26 13:44:03 +0800

    flake.nix: update flake attribute names
    As of v2.7.0, certain attribute names have been renamed. We're just conforming to that.

--- flake.nix
+++ flake.nix
@@ -7,46 +7,43 @@
         nixpkgs.lib.genAttrs [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
     in {
 
-      overlay = final: prev:
-        let guilePackages = prev.callPackages ./pkgs/guile { };
-        in rec {
-          inherit (guilePackages)
-            guile-gnutls guile-gcrypt guile-git guile-json guile-sqlite3
-            guile-lzlib guile-zlib guile-ssh guile-zstd guile-semver guile-avahi
-            guile3-lib;
-          scheme-bytestructures = guilePackages.bytestructures;
+      overlays = {
+        default = final: prev:
+          let guilePackages = prev.callPackages ./pkgs/guile { };
+          in rec {
+            inherit (guilePackages)
+              guile-gnutls guile-gcrypt guile-git guile-json guile-sqlite3
+              guile-lzlib guile-zlib guile-ssh guile-zstd guile-semver
+              guile-avahi guile3-lib;
+            scheme-bytestructures = guilePackages.bytestructures;
 
-          # Guix that comes in all flavors.
-          guix = prev.callPackage ./pkgs/guix.nix { inherit guilePackages; };
-          guix_binary_1_3_0 =
-            prev.callPackage ./pkgs/guix-binary/default.nix { };
-        };
+            # Guix that comes in all flavors.
+            guix = prev.callPackage ./pkgs/guix.nix { inherit guilePackages; };
+            guix_binary_1_3_0 =
+              prev.callPackage ./pkgs/guix-binary/default.nix { };
+          };
+      };
 
       packages = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in self.overlay pkgs pkgs);
-
-      defaultPackage = forAllSystems (system: self.packages.${system}.guix);
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = self.packages.${system}.guix;
+        } // (self.overlays.default pkgs pkgs));
 
       nixosModules = {
         guix = import ./modules/nixos/guix.nix;
         guix-binary = import ./modules/nixos/guix-binary.nix;
       };
 
-      devShell = forAllSystems (system:
-        import ./shell.nix {
-          pkgs = import nixpkgs {
-            overlays = [ self.overlay ];
-            inherit system;
-          };
-        });
-
       devShells = forAllSystems (system:
-        import ./shells {
+        let
           pkgs = import nixpkgs {
             inherit system;
-            overlays = [ self.overlay ];
+            overlays = [ self.overlays.default ];
           };
-        });
+        in {
+          default = import ./shell.nix { inherit pkgs; };
+        } // (import ./shells { inherit pkgs; }));
     };
 }
