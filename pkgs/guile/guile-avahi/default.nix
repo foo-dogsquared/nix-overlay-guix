8a9a650ca62e2835381410f322af0e062a22a7fb
Gabriel Arazas <foo.dogsquared@gmail.com>
2022-08-21 22:47:27 +0800

    guile-avahi: 0.4 -> 0.4-6d43ca
    This is just to make packaging easier. This is also used from the latest
    Guix package definition for its latest revision as of 2022-08-21.

--- pkgs/guile/guile-avahi/default.nix
+++ pkgs/guile/guile-avahi/default.nix
@@ -2,12 +2,12 @@
 
 buildGuileModule rec {
   pname = "guile-avahi";
-  version = "0.4";
+  version = "0.4-6d43ca";
 
   src = fetchgit {
     url = "git://git.sv.gnu.org/guile-avahi.git";
-    rev = "v${version}";
-    sha256 = "sha256-1hlgMU71DPCBn4pZLfV8VE/4grgBsDLhdETj4d8zSNQ=";
+    rev = "6d43caf64f672a9694bf6c98bbf7a734f17a51e8";
+    sha256 = "sha256-qQbPmcVdafqhRntdEJvaPr2r4eyjQYBCOGx7ITpyeTs=";
   };
 
   nativeBuildInputs = [ autoreconfHook pkgconfig texinfo ];
