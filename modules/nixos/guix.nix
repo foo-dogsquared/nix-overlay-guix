1cc6d24efd6bf12596afa2f18af668671be8a800
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-10-19 03:16:21 +0800

    services/guix: update
    * Add garbage collection for Guix service.
    * Add the option to configure number of users for Guix builder user
      pool.
    * Make the services use the package exclusively instead of conditionally
      executing from the root Guix profile. A stray `guix pull` could make
      the Nix-managed Guix installation messy so we're not including that by
      default anymore.
    * Make GUIX_LOCPATH initialized by pam-env instead of the shell (if
      that makes a difference at all).
    
    That makes it the Guix profile activation user service the last thing to
    be done.

--- modules/nixos/guix.nix
+++ modules/nixos/guix.nix
@@ -24,6 +24,7 @@ let
   guixEnv = rec {
     ROOT_PROFILE = "${cfg.stateDir}/guix/profiles/per-user/root/current-guix";
     DAEMON = "${ROOT_PROFILE}/bin/guix-daemon";
+    GUIX = "${ROOT_PROFILE}/bin/guix";
     GUIX_LOCPATH = "${ROOT_PROFILE}/lib/locale";
     LC_ALL = "en_US.utf8";
   };
@@ -57,6 +58,15 @@ in
       description = ''
         The name prefix for the Guix build user pool.
       '';
+    };
+
+    nrBuildUsers = mkOption {
+      type = types.int;
+      description = ''
+        Number of Guix build users to be used in the build pool.
+      '';
+      default = 10;
+      example = 20;
     };
 
     extraArgs = mkOption {
@@ -138,99 +148,137 @@ in
         '';
       };
     };
+
+    gc = {
+      enable = mkEnableOption "automatic garbage collection service for Guix";
+
+      extraFlags = mkOption {
+        type = with types; listOf str;
+        default = [ ];
+        description = ''
+          List of arguments to be passed to {command}`guix gc`.
+        '';
+        example = [ ];
+      };
+
+      dates = lib.mkOption {
+        type = types.str;
+        default = "03:15";
+        example = "weekly";
+        description = ''
+          How often the garbage collection occurs. This takes the time format
+          as indicated from {manpage}`systemd.time(7)`.
+        '';
+      };
+    };
   };
 
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ package ];
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+    {
+      environment.systemPackages = [ package ];
 
-    # Building the user pool for Guix.
-    users.users = guixBuildUsers 10;
-    users.groups."${cfg.group}" = { };
+      # Building the user pool for Guix.
+      users.users = guixBuildUsers cfg.nrBuildUsers;
+      users.groups."${cfg.group}" = { };
 
-    # From this point, the script for services involving activating the Guix
-    # daemon will more likely use the daemon from the Guix root profile which
-    # is present after doing an update (i.e., 'guix pull'). Otherwise, we'll
-    # just use the daemon from the derivation.
-    systemd.services.guix-daemon = {
-      description = "Build daemon for GNU Guix";
-      environment = guixEnv;
-      script = ''
-        if [ ! -x "$DAEMON" ]; then
-          DAEMON="${package}/bin/guix-daemon"
-          GUIX_LOCPATH="${pkgs.glibcLocales}/lib/locale"
-        fi
+      systemd.packages = [ package ];
 
-        exec $DAEMON --build-users-group=${cfg.group} ${
-          lib.escapeShellArgs cfg.extraArgs
-        }
+      # This is to be enabled with their respective submodules.
+      systemd.services.guix-gc.enable = false;
+      systemd.services.guix-publish.enable = false;
+
+      # From this point, the script for services involving activating the Guix
+      # daemon will more likely use the daemon from the Guix root profile which
+      # is present after doing an update (i.e., 'guix pull'). Otherwise, we'll
+      # just use the daemon from the derivation.
+      systemd.services.guix-daemon = {
+        environment = guixEnv;
+        script = ''
+          ${lib.getExe' package "guix-daemon"} --build-users-group=${cfg.group} ${
+            lib.escapeShellArgs cfg.extraArgs
+          }
+        '';
+        serviceConfig = {
+          RemainAfterExit = "yes";
+          StandardOutput = "journal";
+          StandardError = "journal";
+        };
+      };
+
+      # This is based from Nix daemon socket unit from upstream Nix package.
+      # Guix build daemon has support for systemd-style socket activation.
+      systemd.sockets.guix-daemon = {
+        description = "Guix daemon socket";
+        before = [ "multi-user.target" ];
+        listenStreams = [ "${cfg.stateDir}/guix/daemon-socket/socket" ];
+        unitConfig = {
+          RequiresMountsFor = cfg.storeDir;
+          ConditionPathIsReadWrite = "${cfg.stateDir}/guix/daemon-socket";
+        };
+        wantedBy = [ "socket.target" ];
+      };
+
+      # This is to make Guix profiles always be activated at user boot
+      # including Guix home env if you REALLY want to. For now, it only
+      # activates the default Guix user profile, the `current-guix` profile
+      # (from user mode `guix pull`), and Guix home profile. This has the
+      # potential to be configurable but we'll stick with the defaults.
+      #
+      # Whatever else is on the users' discretion. If a user wants to get rid
+      # any of the profiles for whatever reason, those profiles have to be
+      # garbage-collected.
+      #
+      # For more information, see the `activate-guix-profile.scm` script.
+      systemd.user.services.guix-profiles-activation = {
+      };
+
+      # Make transferring files from one store to another easier with the usual
+      # case being of most substitutes from the official Guix CI instance.
+      system.activationScripts.guix = ''
+        ${lib.getExe' package "guix"} archive --authorize < \
+          ${package}/share/guix/ci.guix.gnu.org.pub
       '';
-      serviceConfig = {
-        RemainAfterExit = "yes";
-        StandardOutput = "journal";
-        StandardError = "journal";
-        TasksMax =
-          8192; # See <https://lists.gnu.org/archive/html/guix-devel/2016-04/msg00608.html>.
+
+      # GUIX_LOCPATH is basically LOCPATH but for Guix libc which in turn used by
+      # virtually every Guix-built packages. This is so that Guix-installed
+      # applications wouldn't use incompatible locale data and not touch its host
+      # system.
+      #
+      # Since it is mainly used by Guix-built packages, we'll have to avoid
+      # setting this variable to point to Nix-built locale data.
+      environment.sessionVariables.GUIX_LOCPATH = lib.makeSearchPath "lib/locale" guixProfiles;
+
+      # What Guix profiles export is very similar to Nix profiles so it is
+      # acceptable to list it here. Also, it is more likely that the user would
+      # want to use packages explicitly installed from Guix so we're putting it
+      # first.
+      environment.profiles = lib.mkBefore guixProfiles;
+    }
+
+    (lib.mkIf cfg.publish.enable {
+      systemd.services.guix-publish = {
+        enable = true;
+        script = ''
+          ${lib.getExe' package "guix"} publish --user=${cfg.publish.user} --port=${cfg.publish.port} ${
+            lib.escapeShellArgs cfg.publish.extraArgs
+          }
+        '';
+        serviceConfig = {
+          RemainAfterExit = "yes";
+          StandardOutput = "journal";
+          StandardError = "journal";
+        };
       };
-      wantedBy = [ "multi-user.target" ];
-    };
+    })
 
-    systemd.services.guix-publish = lib.mkIf cfg.publish.enable {
-      description = "Publish the GNU Guix store";
-      environment = guixEnv;
-      script = ''
-        if [ ! -x "$DAEMON" ]; then
-          DAEMON="${package}/bin/guix-daemon"
-          GUIX_LOCPATH="${pkgs.glibcLocales}/lib/locale"
-        fi
-
-        exec $DAEMON publish --user=${cfg.publish.user} --port=${cfg.publish.port} ${
-          lib.escapeShellArgs cfg.publish.extraArgs
-        }
-      '';
-      serviceConfig = {
-        RemainAfterExit = "yes";
-        StandardOutput = "journal";
-        StandardError = "journal";
-        TasksMax =
-          1024; # See <https://lists.gnu.org/archive/html/guix-devel/2016-04/msg00608.html>.
+    (lib.mkIf cfg.gc.enable {
+      systemd.services.guix-gc = {
+        enable = true;
+        startAt = cfg.gc.dates;
+        script = ''
+          ${lib.getExe' package "guix"} gc ${lib.escapeShellArgs cfg.gc.extraArgs}
+        '';
       };
-      wantedBy = [ "multi-user.target" ];
-    };
-
-    # This is based from Nix daemon socket unit from upstream Nix
-    # package.
-    systemd.sockets.guix-daemon = {
-      description = "Guix daemon socket";
-      before = [ "multi-user.target" ];
-      listenStreams = [ "${cfg.stateDir}/guix/daemon-socket/socket" ];
-      unitConfig = {
-        RequiresMountsFor = cfg.storeDir;
-        ConditionPathIsReadWrite = "${cfg.stateDir}/guix/daemon-socket";
-      };
-      wantedBy = [ "socket.target" ];
-    };
-
-    # Make transferring files from one store to another easier with the usual
-    # case being most of them are more likely from the official Guix CI
-    # instance.
-    system.activationScripts.guix = ''
-      ${package}/bin/guix archive --authorize < \
-        ${package}/share/guix/ci.guix.gnu.org.pub
-    '';
-
-    # GUIX_LOCPATH is basically LOCPATH but for Guix libc which in turn used by
-    # virtually every Guix-built packages. This is so that Guix-installed
-    # applications wouldn't use incompatible locale data and not touch its host
-    # system.
-    #
-    # Since it is mainly used by Guix-built packages, we'll have to avoid
-    # setting this variable to point to Nix-built locale data.
-    environment.variables.GUIX_LOCPATH = lib.makeSearchPath "lib/locale" guixProfiles;
-
-    # What Guix profiles export is very similar to Nix profiles so it is
-    # acceptable to list it here. Also, it is more likely that the user would
-    # want to use packages explicitly installed from Guix so we're putting it
-    # first.
-    environment.profiles = lib.mkBefore guixProfiles;
-  };
+    })
+  ]);
 }
