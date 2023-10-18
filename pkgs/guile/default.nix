5636c3906d6721235a8e7b765ae81a71e16297bc
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-10-19 03:05:47 +0800

    guile: remove packages
    They're either upstreamed or improved from nixpkgs.

--- pkgs/guile/default.nix
+++ pkgs/guile/default.nix
@@ -6,20 +6,6 @@ let
       callPackage = newScope self;
     in
     lib.recurseIntoAttrs {
-      bytestructures = callPackage ./bytestructures { };
-      guile-disarchive = callPackage ./disarchive { };
-      guile-avahi = callPackage ./guile-avahi { };
-      guile-gcrypt = callPackage ./guile-gcrypt { };
-      guile-git = callPackage ./guile-git { };
-      guile-json = callPackage ./guile-json { };
-      guile-lzlib = callPackage ./guile-lzlib { };
-      guile-lzma = callPackage ./guile-lzma { };
-      guile-semver = callPackage ./guile-semver { };
-      guile-sqlite3 = callPackage ./guile-sqlite3 { };
-      guile-quickcheck = callPackage ./guile-quickcheck { };
-      guile-ssh = callPackage ./guile-ssh { };
-      guile-zlib = callPackage ./guile-zlib { };
-      guile-zstd = callPackage ./guile-zstd { };
     };
 in
 lib.fix' (lib.extends overrides packages)
